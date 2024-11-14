628fe914f487bd9855601c92ed2d1d20cb26939c
AndreMoreira00 <andremoreira102030@gmail.com>
2024-11-14 18:05:32 -0300

    Sua mensagem de commit

--- Scripts/player/player.gd
+++ Scripts/player/player.gd
@@ -6,13 +6,14 @@ const JUMP_VELOCITY = -400.0
 
 var vida = 5
 var is_attacking = false
+var is_jump = false
 
 
 # Função de física do personagem
 func _ready():
 	# Conectando o sinal de fim de animação ao método
-	$AnimatedSprite2D.connect("animation_finished", Callable(self, "_on_animation_finished"))
-
+	$AnimatedSprite2D.connect("animation_finished", Callable(self, "_on_animation_ataque_finished"))
+	$AnimatedSprite2D.connect("animation_finished", Callable(self, "_on_animation_jump_finished"))
 
 func _physics_process(delta: float) -> void:
 	# Adiciona gravidade
@@ -25,6 +26,8 @@ func _physics_process(delta: float) -> void:
 	# Lógica de pulo
 	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		is_jump = true
+		$AnimatedSprite2D.play("pular")
 	
 	# Lógica de ataque
 	if Input.is_action_just_pressed("shot") and not is_attacking:
@@ -57,11 +60,14 @@ func _get_input():
 	else:
 		$Area2D/HitBox.position.x = 25
 # Função para resetar o estado de ataque quando a animação termina
-func _on_animation_finished():
+func _on_animation_ataque_finished():
 	if $AnimatedSprite2D.animation == "atacando":
 		is_attacking = false
 		$Area2D/HitBox.disabled = true
 
+func _on_animation_jump_finished():
+	if $AnimatedSprite2D.animation == "pular":
+		is_jump = false
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.name == "inimigo_espada":
